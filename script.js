9a4350ff350267480c1cea707715c37434568035
y6377 <yewon6377@gmail.com>
2026-05-10 20:23:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ if(loginBtn){
     window.location.href = "home.html";
   });
 }
+
+/* =========================
+   타이핑 효과
+========================= */
+
+const title = document.getElementById("typing-title");
+
+if(title){
+
+  const text = "PROJECT : ECLIPSE";
+
+  let index = 0;
+
+  function typeText(){
+
+    if(index < text.length){
+
+      title.innerHTML += text.charAt(index);
+
+      index++;
+
+      setTimeout(typeText, 120);
+    }
+  }
+
+  typeText();
+}
